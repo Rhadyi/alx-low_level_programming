bd76ef4becea04fea9e645c0d9b0a875a4b50e9b
Sazed <rhadyisez12@gmail.com>
2022-12-19 21:18:59 +0000

    Code tthat will reverse a certain string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,22 +8,22 @@
  */
 void rev_string(char *s)
 {
-  int i, k, tmp, len;
-
-  i = 0;
-  while (s[i] != '\0')
-    {
-      i++;
-    }
-  k = 0;
-  len = i;
-  i--;
-  while (k < len / 2)
-    {
-      tmp = s[k];
-      s[k] = s[i];
-      s[i] = tmp;
-      i--;
-      k++;
-    }
+	int i, k, tmp, len;
+	
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	k = 0;
+	len = i;
+	i--;
+	while (k < len / 2)
+	{
+	tmp = s[k];
+	s[k] = s[i];
+	s[i] = tmp;
+	i--;
+	k++;
+	}
 }
